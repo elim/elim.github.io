2d5747f73b6539224349d40e7ce515d31cdb32c7
Takeru Naito <takeru.naito@gmail.com>
2019-10-15 11:55:23 +0900

    Update Facebook SDK

---
+++ static/js/tweaks.js
@@ -0,0 +1,8 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '488799897828217',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v4.0'
+  });
+};
